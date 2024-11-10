2188770b87011b05ffaab83812a38445619510e9
pasindu <pramudithapasindu48@gmail.com>
2024-11-10 15:41:47 +0530

    Updated dashboard

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -169,7 +169,7 @@ export default function Dashboard() {
                             </div>
                           </div>
                           <span class="fw-semibold d-block mb-1">Profit</span>
-                          <h3 class="card-title mb-2">$12,628</h3>
+                          <h3 class="card-title mb-2">RS.12,628</h3>
                           <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> +72.80%</small>
                         </div>
                       </div>
@@ -203,7 +203,7 @@ export default function Dashboard() {
                             </div>
                           </div>
                           <span>Sales</span>
-                          <h3 class="card-title text-nowrap mb-1">$4,679</h3>
+                          <h3 class="card-title text-nowrap mb-1">RS.4,679</h3>
                           <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> +28.42%</small>
                         </div>
                       </div>
@@ -249,7 +249,7 @@ export default function Dashboard() {
                             </div>
                             <div class="d-flex flex-column">
                               <small>2022</small>
-                              <h6 class="mb-0">$32.5k</h6>
+                              <h6 class="mb-0">RS.12000</h6>
                             </div>
                           </div>
                           <div class="d-flex">
@@ -258,7 +258,7 @@ export default function Dashboard() {
                             </div>
                             <div class="d-flex flex-column">
                               <small>2021</small>
-                              <h6 class="mb-0">$41.2k</h6>
+                              <h6 class="mb-0">RS.25,000</h6>
                             </div>
                           </div>
                         </div>
@@ -293,7 +293,7 @@ export default function Dashboard() {
                             </div>
                           </div>
                           <span class="d-block mb-1">Payments</span>
-                          <h3 class="card-title text-nowrap mb-2">$2,456</h3>
+                          <h3 class="card-title text-nowrap mb-2">RS.21,456</h3>
                           <small class="text-danger fw-semibold"><i class="bx bx-down-arrow-alt"></i> -14.82%</small>
                         </div>
                       </div>
@@ -323,7 +323,7 @@ export default function Dashboard() {
                             </div>
                           </div>
                           <span class="fw-semibold d-block mb-1">Transactions</span>
-                          <h3 class="card-title mb-2">$14,857</h3>
+                          <h3 class="card-title mb-2">RS.14,857</h3>
                           <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> +28.14%</small>
                         </div>
                       </div>
@@ -341,7 +341,7 @@ export default function Dashboard() {
                                 <small class="text-success text-nowrap fw-semibold"
                                   ><i class="bx bx-chevron-up"></i> 68.2%</small
                                 >
-                                <h3 class="mb-0">$84,686k</h3>
+                                <h3 class="mb-0">RS.84,686</h3>
                               </div>
                             </div>
                             <div id="profileReportChart"></div>
@@ -358,7 +358,7 @@ export default function Dashboard() {
                     <div class="card-header d-flex align-items-center justify-content-between pb-0">
                       <div class="card-title mb-0">
                         <h5 class="m-0 me-2">Order Statistics</h5>
-                        <small class="text-muted">42.82k Total Sales</small>
+                        <small class="text-muted">RS.42,982 Total Sales</small>
                       </div>
                       <div class="dropdown">
                         <button
@@ -482,7 +482,7 @@ export default function Dashboard() {
                             <div>
                               <small class="text-muted d-block">Total Balance</small>
                               <div class="d-flex align-items-center">
-                                <h6 class="mb-0 me-1">$459.10</h6>
+                                <h6 class="mb-0 me-1">RS.45,910</h6>
                                 <small class="text-success fw-semibold">
                                   <i class="bx bx-chevron-up"></i>
                                   42.9%
@@ -497,7 +497,7 @@ export default function Dashboard() {
                             </div>
                             <div>
                               <p class="mb-n1 mt-1">Expenses This Week</p>
-                              <small class="text-muted">$39 less than last week</small>
+                              <small class="text-muted">RS.3900 less than last week</small>
                             </div>
                           </div>
                         </div>
@@ -540,7 +540,7 @@ export default function Dashboard() {
                             </div>
                             <div class="user-progress d-flex align-items-center gap-1">
                               <h6 class="mb-0">+82.6</h6>
-                              <span class="text-muted">USD</span>
+                              <span class="text-muted">RS</span>
                             </div>
                           </div>
                         </li>
@@ -555,7 +555,7 @@ export default function Dashboard() {
                             </div>
                             <div class="user-progress d-flex align-items-center gap-1">
                               <h6 class="mb-0">+270.69</h6>
-                              <span class="text-muted">USD</span>
+                              <span class="text-muted">RS</span>
                             </div>
                           </div>
                         </li>
@@ -570,7 +570,7 @@ export default function Dashboard() {
                             </div>
                             <div class="user-progress d-flex align-items-center gap-1">
                               <h6 class="mb-0">+637.91</h6>
-                              <span class="text-muted">USD</span>
+                              <span class="text-muted">RS</span>
                             </div>
                           </div>
                         </li>
@@ -585,7 +585,7 @@ export default function Dashboard() {
                             </div>
                             <div class="user-progress d-flex align-items-center gap-1">
                               <h6 class="mb-0">-838.71</h6>
-                              <span class="text-muted">USD</span>
+                              <span class="text-muted">RS</span>
                             </div>
                           </div>
                         </li>
@@ -600,7 +600,7 @@ export default function Dashboard() {
                             </div>
                             <div class="user-progress d-flex align-items-center gap-1">
                               <h6 class="mb-0">+203.33</h6>
-                              <span class="text-muted">USD</span>
+                              <span class="text-muted">RS</span>
                             </div>
                           </div>
                         </li>
@@ -615,7 +615,7 @@ export default function Dashboard() {
                             </div>
                             <div class="user-progress d-flex align-items-center gap-1">
                               <h6 class="mb-0">-92.45</h6>
-                              <span class="text-muted">USD</span>
+                              <span class="text-muted">RS</span>
                             </div>
                           </div>
                         </li>
