bd5b72f797e75693911a7592c52721733d6684af
kaveeshbhashitha <kaveeshbhashitha@gmail.com>
2024-11-10 14:55:49 +0530

    Ticket reservation list for user is created.

--- src/pages/web/MyTickets.jsx
+++ src/pages/web/MyTickets.jsx
@@ -1,20 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import Header from '../../components/layout/Header'
 import Footer from '../../components/layout/Footer'
 import FooterRest from '../../components/layout/FooterRest'
 import '../../styles/calendar.css';
+import MyReservation from '../../components/other/MyReservation';
+import useAuthCheck from '../../AuthCheck';
 
 export default function MyTickets() {
+  useAuthCheck();
   const [visibleDiv, setVisibleDiv] = useState(1);
   return (
     <div>
         <Header/>
             <div className='body-calender'>
-                <button className='btn btn-primary' onClick={() => setVisibleDiv(1)}>My Reservation List</button>
-                <button className='btn btn-primary mx-2' onClick={() => setVisibleDiv(2)}>My Reservation Schedule</button>
-
+                <button className='btn btn-primary' onClick={() => setVisibleDiv(1)}><i class="fa-solid fa-list"></i><span className='gap'>My Reservation List</span></button>
+                <button className='btn btn-primary mx-2' onClick={() => setVisibleDiv(2)}><i class="fa-solid fa-calendar-days"></i><span className='gap'>My Reservation Schedule</span></button>
+                <div className='title-margin'>
+                  <h2>My Tickets and Events</h2>
+                </div>
                 <div className={visibleDiv === 1 ? "visible" : "hidden"}>
-                    div1
+                    <MyReservation/>
                 </div>
                 <div className={visibleDiv === 2 ? "visible" : "hidden"}>
                     div2
