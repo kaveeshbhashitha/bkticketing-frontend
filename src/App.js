6f97480ca9464fe77295e73526e2cc9a0068aa9c
pasindu <pramudithapasindu48@gmail.com>
2024-11-13 23:03:12 +0530

    Update AdminSideBar.jsx

--- src/App.js
+++ src/App.js
@@ -20,6 +20,8 @@ import Theater from "./pages/web/Theater";
 import Sports from "./pages/web/Sports";
 import Customer from "./pages/admin/Customer";
 import Notification from "./pages/admin/Notification";
+import Calender from "./components/other/EventCalendar";
+import MyReservation from "./components/other/MyReservation";
 
 function App() {
   return (
@@ -46,6 +48,8 @@ function App() {
             <Route exact path="/Sports" element={<Sports/>}/>
             <Route exact path="/customer" element={<Customer/>}/>
             <Route exact path="/notification" element={<Notification/>}/>
+            <Route exact path="/Calender" element={<Calender/>}/>
+            <Route exact path="/Reservation" element={<MyReservation/>}/>
           </Routes> 
       </Router>
     </div>
